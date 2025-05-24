bdb19ec2f5cb0b016203f9e4908783d7ba5e5561
whqwq <1776678487@qq.com>
2025-05-25 01:18:57 +0800

    update host

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,10 +5,7 @@ import {USER_KEY, TOKEN_NAME} from "../config/global";
 import  router  from '../router';
 import proxy from '../config/proxy';
 const env = import.meta.env.MODE || "development";
-// http://172.17.2.134:10010
-// https://mock.boxuegu.com/mock/3359
-const host = env === 'mock' ? 'https://tjxt-dev.itheima.net/api' : proxy[env].host; // 如果是mock模式 就不配置host 会走本地Mock拦截
-// const host = "http://172.17.2.134/api-test";
+const host = proxy[env].host; // 如果是mock模式 就不配置host 会走本地Mock拦截
 const CODE = {
   LOGIN_TIMEOUT: 1000,
   REQUEST_SUCCESS: 200,
